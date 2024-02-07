e52eeeb1b97285f99d996ffe91a823774f7ddef4
MaruEtto <klimova.mary95@gmail.com>
2024-02-07 11:36:53 +0300

    initial

---
+++ DZ13.4/Helpers.cpp
@@ -0,0 +1,6 @@
+#include "Helpers.h"
+
+int Function(int a, int b)
+{
+	return (a + b) * (a + b);
+}
---
+++ DZ13.4/Main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "Helpers.h"
+
+int main()
+{
+	int result = Function(10, 20);
+	std::cout << "sum = " << result << std::endl;
+}
