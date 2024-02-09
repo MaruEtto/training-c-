fd973caca9ab5b471ff5ee1f878e68648c71ff24
MaruEtto <klimova.mary95@gmail.com>
2024-02-09 16:57:59 +0300

    DZ15.4

--- DZ13.4/Main.cpp
+++ DZ13.4/Main.cpp
@@ -1,15 +1,33 @@
 #include <iostream>
 #include <string>
+using namespace std;
 
 int main()
 {
-	std::string text;
+	//DZ14.4
+
+	/*std::string text;
 	std::cout << "Enter string: ";
 	getline(std::cin, text);
 	std::cout << "Your string: " << text << "\n";
 	std::cout << "String lenght: " << text.length() << "\n";
 	std::cout << "First symbol: " << text.front() << "\n";
-	std::cout << "Last symbol: " << text.back() << "\n";
+	std::cout << "Last symbol: " << text.back() << "\n";*/
+
+	//DZ15.4
+	// Эта программа выводит в консоль все четные числа от 0 до установленного ограничения
+	
+	int Limit;
+
+	cout << "Enter numer limit: " << "\n";
+	cin >> Limit;
+
+	for (int i = 0; i <= Limit; )
+	{
+		cout << i << " ";
+		i += 2;
+	}
+		
 
 	return 0;
 }
